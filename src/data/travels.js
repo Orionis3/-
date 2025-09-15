443391d8a04a15a26593be11e0fc412afdf10764
coder <2933346011@qq.com>
2025-09-15 17:36:15 +0800

    修复数据bug以及引入bug

---
+++ src/data/travels.js
@@ -0,0 +1,20 @@
+// src/data/travels.js
+export default [
+  {
+    id: 1,
+    title: '旅行1',
+    // 补充具体属性，例如：
+    date: '2023-01-01',
+    location: '北京',
+    description: '这是一次北京之旅'
+  },
+  {
+    id: 2,
+    title: '旅行2',
+    // 补充具体属性，例如：
+    date: '2023-05-10',
+    location: '上海',
+    description: '这是一次上海之旅'
+  }
+  // 确保每个对象都有完整的属性，不要使用单独的 ...
+]
